b0d19653d22e68d514432de97e8fbf098d7aabdc
Mohini <mohinikar1497@gmail.com>
2023-12-12 01:27:12 +0530

    added logo and modified title

---
+++ src/pack1/validate_email.java
@@ -0,0 +1,10 @@
+package pack1;
+
+public class validate_email {
+
+	public static void main(String[] args) {
+		System.out.println("validate email");
+
+	}
+
+}
--- src/pack1/validate_logo.java
+++ src/pack1/validate_logo.java
@@ -1,6 +1,6 @@
 package pack1;
 
-public class validate_title {
+public class validate_logo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
